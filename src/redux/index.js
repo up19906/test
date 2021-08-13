6c9aa99c5d26b5d53e965c551f25c58eab43de77
up19906 <33612469+up19906@users.noreply.github.com>
2021-08-13 15:07:49 +0700

    Add files via upload

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from "redux";
 import statehome from "./home/reducer";
 import header from "./header/reducer";
 import concept from "./conceptProposal/reducer";
-import addconcept from "./addconcept/reducer";
+import funding from "./funding/reducer";
 
 export default combineReducers({
   statehome,
   header,
   concept,
-  addconcept,
+  funding,
 });
